f970670f39d0fdb735996c27e6d72190da5053e9
alejandrofontan <fontanvillacampa@gmail.com>
2026-01-03 20:21:41 +1000

    refactor: update setup.py to include the executable as entry point

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='vggt_slam',
     version='0.1.0',
     description='A feedforward SLAM system optimized on the SL(4) manifold.',
-    packages=find_packages(include=['evals', 'evals.*', 'vggt_slam', 'vggt_slam.*']),
+    packages=find_packages(include=['evals', 'evals.*', 'vggt_slam', 'vggt_slam.*', 'vggt', 'vggt.*']),
     
     py_modules=['vslamlab_vggtslam_mono'],
     entry_points={
